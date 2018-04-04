698ac6f2a20c345077a4f0d1df917bf696eccb44
Elie NEHME <elie29@gmail.com>
2018-04-04 18:17:29 +0200

    Used ContainerInterface

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -11,5 +11,5 @@ interface ConfigInterface
     const DI_CACHE_PATH = 'di_cache_path';
     const DI_CACHE_DEFINITION = 'di_cache_definition';
 
-    public function configureContainer(ContainerBuilder $builder) : void;
+    public function configureContainer(ContainerBuilder $builder): void;
 }
--- src/ContainerFactory.php
+++ src/ContainerFactory.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Zend\DI\Config;
 
-use DI\Container;
 use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
 use Zend\DI\Config\ConfigInterface;
 
 class ContainerFactory
 {
-    public function __invoke(ConfigInterface $config) : Container
+
+    public function __invoke(ConfigInterface $config): ContainerInterface
     {
         $builder = new ContainerBuilder();
         $config->configureContainer($builder);
